5384c6f7c9b7074108449bfdf01683730a3b22a1
Leonardo Emerson de Souza <leonardo.emerson@viavarejo.com.br>
2022-04-26 16:50:12 -0300

    versioning

--- SQLiteAbstractCrud.Tests/Test1Field_StrPk_RepositoryTests.cs
+++ SQLiteAbstractCrud.Tests/Test1Field_StrPk_RepositoryTests.cs
@@ -26,81 +26,81 @@ namespace SQLiteAbstractCrud.Tests
         public void MustGet()
         {
             // arrange
-            const string valorFoo = "fooValor";
+            const string field1 = "fooValor";
             var sut = new Test1Field_StrPk_Repository(_pathFileDb);
-            sut.Insert(new Test1Field_StrPk(valorFoo));
+            sut.Insert(new Test1Field_StrPk(field1));
 
             // act
-            var result = sut.Get(valorFoo);
+            var result = sut.Get(field1);
 
             // assert
             Assert.NotNull(result);
-            Assert.AreEqual(valorFoo, result.Field1);
+            Assert.AreEqual(field1, result.Field1);
         }
 
         [Test]
         public void MustInsert()
         {
             // arrange
-            const string valorFoo = "fooValor";
-            var entidade = new Test1Field_StrPk(valorFoo);
+            const string field1 = "fooValor";
+            var entity = new Test1Field_StrPk(field1);
             var sut = new Test1Field_StrPk_Repository(_pathFileDb);
             
             // act
-            sut.Insert(entidade);
+            sut.Insert(entity);
 
             // assert
-            var entidadeInserida = sut.Get(valorFoo);
-            Assert.NotNull(entidadeInserida);
-            Assert.AreEqual(valorFoo, entidadeInserida.Field1);
+            var inserteEntity = sut.Get(field1);
+            Assert.NotNull(inserteEntity);
+            Assert.AreEqual(field1, inserteEntity.Field1);
         }
 
         [Test]
         public void MustGetAll()
         {
             // arrange
-            var entidade1 = new Test1Field_StrPk("fooValor1");
-            var entidade2 = new Test1Field_StrPk("fooValor2");
+            var entity1 = new Test1Field_StrPk("fooValue1");
+            var entity2 = new Test1Field_StrPk("fooValue2");
             var sut = new Test1Field_StrPk_Repository(_pathFileDb);
-            sut.Insert(entidade1);
-            sut.Insert(entidade2);
+            sut.Insert(entity1);
+            sut.Insert(entity2);
             
             // act
-            var entidadesObtidas = sut.GetAll();
+            var actual = sut.GetAll();
 
             // assert
-            Assert.NotNull(entidadesObtidas);
-            Assert.AreEqual(2, entidadesObtidas.Count());
-            Assert.True(entidadesObtidas.FirstOrDefault(x => x.Field1 == entidade1.Field1) != null);
-            Assert.True(entidadesObtidas.FirstOrDefault(x => x.Field1 == entidade2.Field1) != null);
+            Assert.NotNull(actual);
+            Assert.AreEqual(2, actual.Count());
+            Assert.True(actual.FirstOrDefault(x => x.Field1 == entity1.Field1) != null);
+            Assert.True(actual.FirstOrDefault(x => x.Field1 == entity2.Field1) != null);
         }
 
         [Test]
         public void MustInsertInBatch()
         {
             // arrange
-            var valor1 = "fooValor1";
-            var valor2 = "fooValor2";
-            var valor3 = "fooValor3";
-            var entidade1 = new Test1Field_StrPk(valor1);
-            var entidade2 = new Test1Field_StrPk(valor2);
-            var entidade3 = new Test1Field_StrPk(valor3);
+            var value1 = "fooValue1";
+            var value2 = "fooValue2";
+            var value3 = "fooValue3";
+            var entity1 = new Test1Field_StrPk(value1);
+            var entity2 = new Test1Field_StrPk(value2);
+            var entity3 = new Test1Field_StrPk(value3);
             
             var sut = new Test1Field_StrPk_Repository(_pathFileDb);
             
             // act
-            sut.InsertBatch(new List<Test1Field_StrPk>{ entidade1, entidade2, entidade3 });
+            sut.InsertBatch(new List<Test1Field_StrPk>{ entity1, entity2, entity3 });
 
             // assert
-            var entidadeInserida1 = sut.Get(valor1);
-            var entidadeInserida2 = sut.Get(valor2);
-            var entidadeInserida3 = sut.Get(valor3);
-            Assert.NotNull(entidadeInserida1);
-            Assert.NotNull(entidadeInserida2);
-            Assert.NotNull(entidadeInserida3);
-            Assert.AreEqual(valor1, entidade1.Field1);
-            Assert.AreEqual(valor2, entidade2.Field1);
-            Assert.AreEqual(valor3, entidade3.Field1);
+            var insertedEntity1 = sut.Get(value1);
+            var insertedEntity2 = sut.Get(value2);
+            var insertedEntity3 = sut.Get(value3);
+            Assert.NotNull(insertedEntity1);
+            Assert.NotNull(insertedEntity2);
+            Assert.NotNull(insertedEntity3);
+            Assert.AreEqual(value1, entity1.Field1);
+            Assert.AreEqual(value2, entity2.Field1);
+            Assert.AreEqual(value3, entity3.Field1);
         }
     }
 
